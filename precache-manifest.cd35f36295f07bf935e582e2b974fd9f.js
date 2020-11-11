ca87e5106b051c0fb32d327ab74a0303e3df9259
danielm2402 <damunoz216@unicauca.edu.co>
2020-11-10 19:14:52 -0500

    Updates

---
+++ precache-manifest.cd35f36295f07bf935e582e2b974fd9f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c2a2b62cb839187a23c3d68e4e19fe0",
+    "url": "/danielm2402/acoplados/index.html"
+  },
+  {
+    "revision": "3b3842fd1aa12aac5d21",
+    "url": "/danielm2402/acoplados/static/css/main.ad601555.chunk.css"
+  },
+  {
+    "revision": "2b2d10503687b6980c6e",
+    "url": "/danielm2402/acoplados/static/js/2.6e8c8f74.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/danielm2402/acoplados/static/js/2.6e8c8f74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b3842fd1aa12aac5d21",
+    "url": "/danielm2402/acoplados/static/js/main.e1162edc.chunk.js"
+  },
+  {
+    "revision": "6bb5be0fd1c220fb4067",
+    "url": "/danielm2402/acoplados/static/js/runtime-main.25c16346.js"
+  },
+  {
+    "revision": "e46a7579c0d4713e95b3e785cd520d18",
+    "url": "/danielm2402/acoplados/static/media/bg1.e46a7579.jpg"
+  },
+  {
+    "revision": "46c908607ed4aa89ef4da2ebea7735d9",
+    "url": "/danielm2402/acoplados/static/media/gif-pendulum.46c90860.gif"
+  }
+]);
