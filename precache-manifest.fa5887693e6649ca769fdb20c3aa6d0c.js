197233b9926b13db38d6db1e985a8abe7f65e6b6
danielm2402 <damunoz216@unicauca.edu.co>
2020-11-10 19:54:53 -0500

    Updates

---
+++ precache-manifest.fa5887693e6649ca769fdb20c3aa6d0c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "651241848e6fc31b74ba215f1da8077b",
+    "url": "/acoplados/index.html"
+  },
+  {
+    "revision": "39fdd46bedc490a37311",
+    "url": "/acoplados/static/css/main.8216a368.chunk.css"
+  },
+  {
+    "revision": "4f066c990f95eeed9f20",
+    "url": "/acoplados/static/js/2.e12de0bd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/acoplados/static/js/2.e12de0bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39fdd46bedc490a37311",
+    "url": "/acoplados/static/js/main.9a4826b9.chunk.js"
+  },
+  {
+    "revision": "cb936091bddee72240ce",
+    "url": "/acoplados/static/js/runtime-main.1d4c8dba.js"
+  },
+  {
+    "revision": "e46a7579c0d4713e95b3e785cd520d18",
+    "url": "/acoplados/static/media/bg1.e46a7579.jpg"
+  },
+  {
+    "revision": "46c908607ed4aa89ef4da2ebea7735d9",
+    "url": "/acoplados/static/media/gif-pendulum.46c90860.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/danielm2402/acoplados/precache-manifest.4d23290ecbab1e76ef47e60c06fc80e0.js"
+  "/acoplados/precache-manifest.fa5887693e6649ca769fdb20c3aa6d0c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/danielm2402/acoplados/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/acoplados/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
